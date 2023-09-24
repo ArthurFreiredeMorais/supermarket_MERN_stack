c244ec4d2b4e58538e62fa497096b46d991912c7
ArthurFreiredeMorais <arthurfreiredemorais@gmail.com>
2023-09-24 15:40:57 -0300

    Loader and message components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,8 @@
 import { Row, Col } from "react-bootstrap";
 import { useGetProductsQuery } from "../slices/productsApiSlice"; // Import the query hook from the productSlice
 import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 // Define the HomeScreen component
 const HomeScreen = () => {
@@ -10,18 +12,27 @@ const HomeScreen = () => {
   return (
     <>
       {isLoading ? ( // Display a loading message while data is loading
-        <div>Loading...</div>
+        <Loader />
       ) : error ? ( // Display an error message if there is an error
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
-        <> {/* Display product data if there are no errors */}
+        <>
+          {" "}
+          {/* Display product data if there are no errors */}
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => ( // Map through the products and display them in a grid
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} /> {/* Render a Product component for each product */}
-              </Col>
-            ))}
+            {products.map(
+              (
+                product // Map through the products and display them in a grid
+              ) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />{" "}
+                  {/* Render a Product component for each product */}
+                </Col>
+              )
+            )}
           </Row>
         </>
       )}
@@ -30,4 +41,3 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen; // Export the HomeScreen component
-
