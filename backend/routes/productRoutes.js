cf55211000a3c73a1c721e123d9ca360b70f1b47
ArthurFreiredeMorais <arthurfreiredemorais@gmail.com>
2023-09-24 02:46:02 -0300

    Starting controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
 
-// Get all products
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
-// Get product by id
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.json(product);
-    }
-    res.status(404);
-    throw new Error('Resource not found');
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
 
