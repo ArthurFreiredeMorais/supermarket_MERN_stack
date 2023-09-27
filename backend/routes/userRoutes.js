dca912578219de486b477b172c5df970a69d7aba
ArthurFreiredeMorais <arthurfreiredemorais@gmail.com>
2023-09-27 11:59:29 -0300

    cookies implemented

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,13 +9,20 @@ import {
   getUserById,
   updateUser,
 } from '../controllers/userController.js';
+import { protect, employee } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, employee, getUsers);
 router.post('/auth', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-
-router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, employee, deleteUser)
+  .get(protect, employee, getUserById)
+  .put(protect, employee, updateUser);
 
 export default router;
