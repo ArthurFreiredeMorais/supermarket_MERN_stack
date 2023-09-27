9a65414a386db99d29e7888b61d570f230c0d9c9
ArthurFreiredeMorais <arthurfreiredemorais@gmail.com>
2023-09-27 11:34:52 -0300

    added login function

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -11,8 +12,12 @@ connectDB();
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
